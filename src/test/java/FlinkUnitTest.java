f98b12664f4cc884b4c47b75c478f21ebb635604
Jimmy <jimmy2002916@gmail.com>
2020-04-16 15:16:31 +0800

    Update FlinkUnitTest.java

--- src/test/java/FlinkUnitTest.java
+++ src/test/java/FlinkUnitTest.java
@@ -1,21 +1,18 @@
-import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.SinkFunction;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumerBase;
-import org.junit.ClassRule;
 import org.junit.Test;
 import static org.easymock.EasyMock.*;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import org.easymock.*;
-import java.util.stream.Stream;
 
 public class FlinkUnitTest {
-
+    
     @Test
     public void unitTestFlinkApplication() throws Exception{
-        FlinkApplication flinkUnitTest = mock(FlinkApplication.class);
+        
+        Application flinkUnitTest = mock(FlinkApplication.class);
         flinkUnitTest.flinkToMysql(
                 EasyMock.isA(StreamExecutionEnvironment.class),
                 EasyMock.isA(FlinkKafkaConsumerBase.class),
@@ -32,8 +29,3 @@ public class FlinkUnitTest {
         
     }
 }
-
-
-
-
-
